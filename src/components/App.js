364373d2fb5fc50148b62e01b3fe63e4cc69320a
Orisabiyi <orisabiyidavid@gmail.com>
2024-05-17 03:51:46 +0100

    styled the input form, made it a controlled element and make it update the todo state that is passed down as a props into the CreateTodo component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import AppTitle from "./AppTitle";
 import CreateTodo from "./CreateTodo";
 
 export default function App() {
+  const [todo, setTodo] = useState([]);
+
+  function handleTodo(newTodo) {
+    setTodo((todos) => [...todos, newTodo]);
+  }
   return (
     <div className="app">
       <div className="app-container">
         <AppTitle />
-        <CreateTodo />
+
+        <CreateTodo onTodo={handleTodo} />
       </div>
     </div>
   );
--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -1,8 +1,27 @@
-export default function CreateTodo() {
+import { useState } from "react";
+
+export default function CreateTodo({ onTodo }) {
+  const [task, setTask] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const todo = { task, id: crypto.randomUUID(), completed: false };
+
+    // add task to todo list
+    onTodo(todo);
+
+    setTask("");
+  }
+
   return (
-    <form className="create-todo">
+    <form className="create-todo" onSubmit={handleSubmit}>
       <div className="check-todo"></div>
-      <input type="text" placeholder="Create a todo..." />
+      <input
+        type="text"
+        placeholder="Create a new todo..."
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
     </form>
   );
 }
